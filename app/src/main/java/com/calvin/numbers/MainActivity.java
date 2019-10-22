39661fe5d68dccd6ff8d20343453bc03ef6bee85
miia <miia@com.tw>
2019-10-22 17:10:46 +0800

    6666

--- app/src/main/java/com/calvin/numbers/MainActivity.java
+++ app/src/main/java/com/calvin/numbers/MainActivity.java
@@ -1,40 +1,60 @@
 package com.calvin.numbers;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private TextView numbers;
-
+    int counter;
+    int secret = new Random().nextInt(10) + 1;
+    ImageView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("mainActive","secret" + secret);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         numbers=findViewById(R.id.num);
+        result =findViewById(R.id.result);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int num= Integer.parseInt(numbers.getText().toString());
-                num++;
-                numbers.setText(""+num);
+                counter++;
+                numbers.setText(String.valueOf(counter));
+                result.setVisibility(View.VISIBLE);
+                if (counter == secret) {
+                    Toast.makeText(MainActivity.this, "Whats up", Toast.LENGTH_LONG).show();
+                    result.setImageResource(R.drawable.shocked);
+                }else{
+                    result.setImageResource(R.drawable.smile);
+
+                }
             }
         });
     }
             public void button(View view){
-           numbers.setText("0");
+            counter=0;
+            numbers.setText(String.valueOf(counter));
 
             }
 
